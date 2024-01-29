ec0496dbd70a034387394ee060ad1baaca4715ed
nirmala-devik <nd12121993@gmail.com>
2024-01-29 10:16:28 +0530

    Recursion

---
+++ src/Oops_Concepts/Recursion_Fibonacci.java
@@ -0,0 +1,24 @@
+package Oops_Concepts;
+
+public class Recursion_Fibonacci {
+   static int n1=0,n2=1,n3=0;
+   static void fibonacci(int count) {
+       if (count > 0) {
+           n3 = n1 + n2;
+           n1 = n2;
+           n2 = n3;
+           System.out.println(" " + n3);
+           fibonacci(count - 1);
+       }
+   }
+           public static void main(String[] args){
+               int count=10;
+               System.out.println(n1+" "+n3);
+               fibonacci(count-2);
+
+       }
+       }
+
+
+
+
